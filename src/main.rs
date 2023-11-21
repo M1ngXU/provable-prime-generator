68c21f8f478fef1549a3f06f628fbe860bbdca2a
MingXu <91617361+M1ngXU@users.noreply.github.com>
2023-11-22 00:24:57 +0100

    speedups, using simd for faster cmp, optimized params

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-#![feature(concat_idents)]
+#![feature(concat_idents, portable_simd)]
 
 use std::convert::identity;
 use std::fs::File;
 use std::hint::black_box;
 use std::io::{stdout, Write};
+use std::simd::Simd;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
-use numext_fixed_uint::{u2048 as int, U2048};
+use numext_fixed_uint::U2048;
 use primes::*;
 use rand::{thread_rng, Rng};
 use rayon::iter::{IntoParallelIterator, ParallelBridge, ParallelIterator};
@@ -65,35 +66,36 @@ fn power2(a: u64) -> Int {
     out.set_bit(a as usize, true);
     out
 }
-/// Random int between `a`, `b` with `a < b`
-fn random(a: &Int, b: &Int) -> Int {
+/// Random int between `0` and `a`
+fn random(a: &Int) -> Int {
     let mut out = Int::zero();
     let mut rng = thread_rng();
-    let ba = (b - a).0;
     // better branch prediction, handwritten asm?
     let mut i = 0;
-    for (j, ba) in ba.iter().enumerate() {
+    for (j, ba) in a.0.iter().enumerate() {
         if ba != &0 {
             i = j;
+            // break; // this line adds 20% to the runtime?? (also add `.rev()` after `.enumerate()`)
         }
     }
-    out.0[i] = rng.gen_range(0..ba[i]);
+    out.0[i] = rng.gen_range(0..a.0[i]);
     rng.fill(&mut out.0[..i]);
-    add(a, &out)
+    out
 }
 /// Checks whether a prime less than `b` divides `a`
 ///
-/// - `b` must be less than or equals 2<sup>16</sub> (enough for `k <= 1024` and correct `C_OPT_D`)
+/// - `b` must be less than or equals 2<sup>P0/2</sub> (this actually only checks for primes less than the last power of 2 of b and less than P0/2)
 fn trial_division(a: &Int, b: u64) -> bool {
-    debug_assert!(b <= 1 << 16, "`b` too big, {b}");
+    debug_assert!(b <= 1 << (P0 / 2), "`b` too big, {b}");
     debug_assert!(&Int::from(b) < a, "`b` greater a, {b} >= {a}");
     PRIMES
-        .iter() // avoid `.into_iter()` to prevent stack overflow
-        .take_while(|p| p < &&b)
-        .all(|p| (a % p).0[0] != 0)
+        .iter()
+        .take(PRIMES_UNTIL_2K[u64::BITS as usize - b.leading_zeros() as usize - 1])
+        .all(|p| (a % p).0[0] != 0) // p < u64::MAX so fine
 }
 /// Generates a number between `0.5` and `max`, see Appendix 1
 fn generate_relative_size(max: f64) -> f64 {
+    // return 0.5; // sacrifice prime distribution for correct (probably really bad)
     if max <= 0.5 {
         0.5
     } else {
@@ -104,13 +106,14 @@ fn generate_relative_size(max: f64) -> f64 {
         2.0f64.powf(r - 1.0)
     }
 }
-/// Calculates a<sup>n</sup> mod m
-fn pow_mod_n(a: &Int, n: &Int, m: &Int) -> Int {
-    let mut x = a.clone();
+/// Calculates a<sup>n + additional</sup> mod m
+fn pow_mod_n(mut x: Int, n: &Int, m: &Int, additional: usize) -> Int {
     let mut y = Int::one();
-    for i in 0..Int::count_bits() as usize - n.leading_zeros() as usize - 1 {
+    for i in 0..Int::count_bits() as usize - n.leading_zeros() as usize - 1 + additional {
         // only least significant bit relevant for odd check
-        if n.bit(i) == Some(true) {
+        //
+        // if underflow then the index is too big and returns none (would be not odd in that case)
+        if n.bit(i.wrapping_sub(additional)) == Some(true) {
             y = mul(&x, &y) % m;
         }
         x = mul(&x, &x) % m;
@@ -118,8 +121,9 @@ fn pow_mod_n(a: &Int, n: &Int, m: &Int) -> Int {
 
     mul(&x, &y) % m
 }
+
 /// Checks whether lemma 1 is true with `F = q`
-fn check_lemma1(n: &Int, a: &Int, r: Int, q: &Int) -> bool {
+fn check_lemma1(n: &Int, a: Int, r: Int, q: &Int) -> bool {
     // (1) a^(n - 1) = 1 mod n
     // (2) gcd(a^((n - 1) / q) - 1, n) = 1
     //
@@ -130,8 +134,11 @@ fn check_lemma1(n: &Int, a: &Int, r: Int, q: &Int) -> bool {
     // (n - 1) / q = 2R
     // (2) gcd(a^(2R) - 1, n) = 1
 
-    let anq = pow_mod_n(a, &(r << 1), n);
-    (&anq - Int::one()).gcd(n) == Int::one() && pow_mod_n(&anq, q, n) == Int::one()
+    let anq = pow_mod_n(a, &r, n, 1);
+    let anq1 = sub(&anq, &Int::one());
+
+    Simd::from(pow_mod_n(anq, q, n, 0).0) == Simd::from(Int::one().0)
+        && Simd::from(anq1.gcd(n).0) == Simd::from(Int::one().0)
 }
 
 // constants
@@ -183,19 +190,18 @@ fn fast_prime(k: u64) -> Option<Int> {
         #[allow(non_snake_case)]
         let I = power2(k - 1) / &q;
         let g = C_OPT_N * k * k / C_OPT_D;
-        #[allow(non_snake_case)]
-        let I2 = &I << 1;
         n = std::iter::repeat(false)
             // .take(2000) // use this to limit tries, this results in fails
             .par_bridge()
             .into_par_iter()
             .map(|_| {
-                let r = random(&I, &I2);
+                let r = add(&I, &random(&I));
                 let mut n = mul(&r, &q2);
-                let a = random(&int!("2"), &n);
+                let mut a = random(&n);
+                a.0[0] |= 0b10; // make sure that `a` is at least 2, so a \in [2, n)
                 n.0[0] += 1; // this can't overflow since rq2 is even (u64::MAX is odd)
 
-                (trial_division(&n, g) && check_lemma1(&n, &a, r, &q)).then_some(n)
+                (trial_division(&n, g) && check_lemma1(&n, a, r, &q)).then_some(n)
             })
             .find_map_any(identity)?;
         // .find_map(identity)?; // use this for single threaded
@@ -209,6 +215,13 @@ fn add(a: &Int, b: &Int) -> Int {
     return a + b;
     #[cfg(feature = "ignore-overflow")]
     return a.overflowing_add(b).0;
+}
+#[inline(always)]
+fn sub(a: &Int, b: &Int) -> Int {
+    #[cfg(not(feature = "ignore-overflow"))]
+    return a - b;
+    #[cfg(feature = "ignore-overflow")]
+    return a.overflowing_sub(b).0;
 }
 #[inline(always)]
 fn mul(a: &Int, b: &Int) -> Int {
